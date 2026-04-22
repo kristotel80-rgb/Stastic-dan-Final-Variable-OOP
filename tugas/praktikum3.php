dd98f758664f3a5c8a68329ef42dd7b3cda35447
gratia <kristotel80@gmail.com>
2026-04-22 16:38:23 +0700

    after

--- tugas/praktikum3.php
+++ tugas/praktikum3.php
@@ -3,24 +3,47 @@
 class Produk {
 
     public static $jumlahProduk = 0;
+    public $nama;
+    public $harga;
 
-    public function tambahProduk() {
+    public function __construct($nama, $harga) {
+        $this->nama = $nama;
+        $this->harga = $harga;
         self::$jumlahProduk++;
+    }
+
+    public function tampilProduk() {
+        echo "Nama: " . $this->nama . " | Harga: " . $this->harga . "<br>";
     }
 
 }
 
 class Transaksi {
 
-    final public function prosesTransaksi() {
-        echo "Transaksi diproses";
+    final public function prosesTransaksi($produkList) {
+        echo "<br>=== Transaksi Diproses ===<br>";
+        $total = 0;
+
+        foreach ($produkList as $produk) {
+            $produk->tampilProduk();
+            $total += $produk->harga;
+        }
+
+        echo "Total Bayar: " . $total . "<br>";
     }
 
 }
 
-$p1 = new Produk();
-$p1->tambahProduk();
+// Minimal 3 produk
+$p1 = new Produk("Buku", 5000);
+$p2 = new Produk("Pulpen", 2000);
+$p3 = new Produk("Penghapus", 1000);
 
-echo "Total Produk: " . Produk::$jumlahProduk;
+// Tampilkan total produk
+echo "Total Produk: " . Produk::$jumlahProduk . "<br>";
+
+// Simulasi transaksi
+$transaksi = new Transaksi();
+$transaksi->prosesTransaksi([$p1, $p2, $p3]);
 
 ?>
